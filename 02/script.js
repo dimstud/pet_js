e2edbf9808fc87778596b06ec52193c49c8da9dd
Dmitry Ageev <dimanagee@yandex.ru>
2024-10-14 11:39:56 +0300

    start

--- 02/script.js
+++ 02/script.js
@@ -1,2 +1,15 @@
 "use strict";
+const arr = [1, 2, 3];
+const obj = {
+        1: 1,
+        b:2,
+      };
 
+obj.c = 4;
+console.log(obj);
+
+
+const obj2 = {
+        Alice: 1,
+        Anna: 2,
+      };
